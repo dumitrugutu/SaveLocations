9866327341bace96ab3dc8e0769d1fdc222c28bb
Dumitru Gutu <dumitrugutu88@gmail.com>
2017-12-07 16:01:44 -0600

    Connect the buttons and outlets

--- SaveLocations/CurrentLocationViewController.swift
+++ SaveLocations/CurrentLocationViewController.swift
@@ -9,15 +9,21 @@
 import UIKit
 
 class CurrentLocationViewController: UIViewController {
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longitudeLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var tagLabel: UIButton!
+    @IBOutlet weak var getButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func getLocation() {
+        //TODO: - get location
     }
 
 
