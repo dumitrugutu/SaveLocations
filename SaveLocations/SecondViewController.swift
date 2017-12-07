f5bbec7bbd7384798e77bb2faa813fb9eb75656b
Dumitru Gutu <dumitrugutu88@gmail.com>
2017-12-07 15:35:36 -0600

    Initial Commit

---
+++ SaveLocations/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  SaveLocations
+//
+//  Created by Dumitru Gutu on 12/7/17.
+//  Copyright © 2017 Dumitru Gutu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
