e983fd26f7fb7459d91f8e835a6e9b20a202c27d
Dumitru Gutu <dumitrugutu88@gmail.com>
2017-12-08 15:47:00 -0600

    Display alert when access to current location is denied

--- SaveLocations/CurrentLocationViewController.swift
+++ SaveLocations/CurrentLocationViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreLocation
 
 class CurrentLocationViewController: UIViewController {
     
@@ -16,6 +17,8 @@ class CurrentLocationViewController: UIViewController {
     @IBOutlet weak var addressLabel: UILabel!
     @IBOutlet weak var tagLabel: UIButton!
     @IBOutlet weak var getButton: UIButton!
+    
+    let locationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,9 +26,46 @@ class CurrentLocationViewController: UIViewController {
     }
 
     @IBAction func getLocation() {
-        //TODO: - get location
+        let authStatus = CLLocationManager.authorizationStatus()
+        
+        if authStatus == .notDetermined {
+            locationManager.requestWhenInUseAuthorization()
+            return
+        }
+        
+        if authStatus == .denied || authStatus == .restricted {
+            showLocationServiceDeniedAlert()
+            return
+        }
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        locationManager.startUpdatingLocation()
     }
-
+    
+    func showLocationServiceDeniedAlert() {
+        let alert = UIAlertController(title: "Location Services Disabled", message: "Please enable location services for this app in the Settings.", preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        
+        alert.addAction(okAction)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
 
 }
+
+//MARK: - CLLocationManagerDelegate
+extension CurrentLocationViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("didFailWithError \(error)")
+    }
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let newLocation = locations.last!
+        print("didUpdateLocations \(newLocation)")
+    }
+}
+
 
