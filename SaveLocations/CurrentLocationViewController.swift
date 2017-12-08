ae5b8a399e41c169dc924d0d915815529f26ed2d
Dumitru Gutu <dumitrugutu88@gmail.com>
2017-12-08 16:01:40 -0600

    Update labels when getting a location

--- SaveLocations/CurrentLocationViewController.swift
+++ SaveLocations/CurrentLocationViewController.swift
@@ -15,10 +15,11 @@ class CurrentLocationViewController: UIViewController {
     @IBOutlet weak var latitudeLabel: UILabel!
     @IBOutlet weak var longitudeLabel: UILabel!
     @IBOutlet weak var addressLabel: UILabel!
-    @IBOutlet weak var tagLabel: UIButton!
+    @IBOutlet weak var tagButton: UIButton!
     @IBOutlet weak var getButton: UIButton!
     
     let locationManager = CLLocationManager()
+    var location: CLLocation?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -53,19 +54,37 @@ class CurrentLocationViewController: UIViewController {
         present(alert, animated: true, completion: nil)
     }
     
-    
+    func updateLabels() {
+        if let location = location {
+            latitudeLabel.text = String(format: "%.8f", location.coordinate.latitude)
+            longitudeLabel.text = String(format: "%.8f", location.coordinate.longitude)
+            tagButton.isHidden = false
+            messageLabel.text = ""
+        } else {
+            latitudeLabel.text = ""
+            longitudeLabel.text = ""
+            addressLabel.text = ""
+            tagButton.isHidden = true
+            messageLabel.text = "Tap 'Get My Location' to start"
+        }
+    }
 
 }
 
 //MARK: - CLLocationManagerDelegate
 extension CurrentLocationViewController: CLLocationManagerDelegate {
+    
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         print("didFailWithError \(error)")
     }
+    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let newLocation = locations.last!
         print("didUpdateLocations \(newLocation)")
+        location = newLocation
+        updateLabels()
     }
+    
 }
 
 
